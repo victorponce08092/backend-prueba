7889331ee15de5c310128bdd895b867db5b46c70
Victor Ponce <padgiosoftware@gmail.com>
2025-09-28 23:19:11 -0600

    Descripción de mis cambios

--- index.js
+++ index.js
@@ -10,10 +10,16 @@ import crypto from "crypto";
 
 import designsRouter from "./designs.routes.js";
 
-app.use(cors({
-  origin: "*"
-}));
+const allowedOrigins = [
+  "http://localhost:5001",   // tu frontend local
+];
 
+
+const app = express();
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true, // si usas cookies/tokens
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); // Para Twilio webhooks (x-www-form-urlencoded)
 
